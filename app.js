90385ec92142ed964046b1eab0ab06243f3d485e
Abimbola Adesoye <bimboadesoye44@gmail.com>
2023-04-28 15:19:06 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let logIn = document.querySelector(".log-in");
+let emailInput = document.querySelector(".email-input");
+let passInput = document.querySelector(".pass-input");
+
+logIn.addEventListener("click", () => {
+  if (emailInput.value.length === 0 || passInput.value.length === 0) {
+    alert("All input fields should be filled");
+    return;
+  }
+  if (passInput.value.length < 6) {
+    alert("Passord length must be more than 6.");
+    return;
+  }
+  alert("Registration completed.");
+});
